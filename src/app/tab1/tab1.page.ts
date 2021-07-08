3bacfb96bc63cb2ec19f9767795e79b2db63c3ca
Purvansha Annapurne <purvanshaa.cseb19@sbjit.edu.in>
2021-07-08 22:48:09 +0530

    ionic app

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+// { Plugins } from '@capacitor/core';
+
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+  photo: SafeResourceUrl;
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  async takePicture() {
+    const image = await Camera.getPhoto({
+      quality: 100,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Camera
+    });
+    this.photo=this.sanitizer.bypassSecurityTrustResourceUrl(image && (image.dataUrl));
+  }
+
+}
